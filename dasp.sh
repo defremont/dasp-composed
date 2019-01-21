db8309fdc5ebd82c21c52d99033372dfe870da70
defremont <andre.def93@gmail.com>
2019-01-21 14:19:17 -0300

    start

---
+++ dasp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+./fabric-dev-servers/stopFabric.sh && ./fabric-dev-servers/teardownFabric.sh
+
+composer card delete --card admin@dasp-net && composer card delete --card PeerAdmin@hlfv1
+
+docker rm $(docker ps -a -q) -f
+
+./fabric-dev-servers/startFabric.sh && ./fabric-dev-servers/createPeerAdminCard.sh
+
+cd ./dasp-hyperledger
+composer archive create -t dir -n .
+composer network install --card PeerAdmin@hlfv1 --archiveFile dasp-net@0.0.1.bna
+composer network start --networkName dasp-net --networkVersion 0.0.1 --networkAdmin admin --networkAdminEnrollSecret adminpw --card PeerAdmin@hlfv1 --file networkadmin.card
+composer card import --file networkadmin.card
+composer network ping --card admin@dasp-net 
+
+cd ..
+
+npm install --prefix ./composer-playground & npm install --prefix ./composer-playground-api
+
+npm start --prefix ./composer-playground & npm start --prefix ./composer-playground-api
+
+
